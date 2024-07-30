c860e1580287ea5f624de77badac458f71c927e8
PRAJAPATI HARSH <harshprajapati8583@gmail.com>
2024-07-30 14:33:01 +0530

    change config file

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    host:"192.168.240.76"
-  }
+  // server:{
+  //   host:"192.168.240.76"
+  // }
 })
